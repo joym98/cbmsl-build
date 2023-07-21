47632c800b04cab6fa5c8965afbcb58272288d2b
joym98 <joy.m@vixplor.com>
2023-07-21 11:10:45 +0530

    idia power

---
+++ build/precache-manifest.ea1c6efd93366f1da4b9c997df986fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d81092ee69bb18eed9c4a35b0eb4907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d2a013f0b58d25b67c",
+    "url": "/static/css/2.c0599547.chunk.css"
+  },
+  {
+    "revision": "9b25a2d898ccddcb9494",
+    "url": "/static/css/main.0f712661.chunk.css"
+  },
+  {
+    "revision": "89d2a013f0b58d25b67c",
+    "url": "/static/js/2.4b4b5970.chunk.js"
+  },
+  {
+    "revision": "62004945c8cf54a3f3ce30e6f5eefd75",
+    "url": "/static/js/2.4b4b5970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b25a2d898ccddcb9494",
+    "url": "/static/js/main.ddd8809d.chunk.js"
+  },
+  {
+    "revision": "522faa865e76f0b2e52c",
+    "url": "/static/js/runtime-main.30bb2e4f.js"
+  }
+]);
